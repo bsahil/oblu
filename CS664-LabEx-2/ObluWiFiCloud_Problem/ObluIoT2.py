1dcf175bb376d514de948e55717800473d285ac3
bsahil <bsahil012@gmail.com>
2018-07-15 23:25:04 +0530

    DeviceClient update

--- CS664-LabEx-2/ObluWiFiCloud_Problem/ObluIoT2.py
+++ CS664-LabEx-2/ObluWiFiCloud_Problem/ObluIoT2.py
@@ -29,7 +29,6 @@
 #   06-06-2018 |    0.0       |   Ajit Gupta                     | TCP/IP communication with oblu
 
 import pyrebase
-# from pyrebase import pyrebase
 import Queue
 import struct
 import math
@@ -57,8 +56,8 @@ PACKET_LEN = 64
 START_CMD = "340034"
 SYS_OFF_CMD = "220022"
 PROC_OFF_CMD = "320032"
-LOG_FILE = "steps.txt" #check in powershell, > Get-Content -Path "C:/xampp/htdocs/iot_single/steps" -Wait
-LOG_FILE2 = "steps1.txt"
+# LOG_FILE = "steps.txt" #check in powershell, > Get-Content -Path "C:/xampp/htdocs/iot_single/steps" -Wait
+# LOG_FILE2 = "steps1.txt"
 TIME_OUT = 10
 NUM_RETRY = -1
 g_isRunning = True
@@ -117,10 +116,11 @@ class DeviceConnectivityError(RuntimeError):
 class DeviceClient(threading.Thread):
     client_socket = ''
     outfile = ''
-    def __init__(self, name, ip, port, queue):
+    def __init__(self, name, ip, port, queue, stepFile):
         threading.Thread.__init__(self)
         self.host = ip
         self.port = port
+        self.stepFile = stepFile
         self.pkt_counter = 0
         self.last_ack = ''
         self.filename = name
@@ -260,16 +260,16 @@ class DeviceClient(threading.Thread):
         return self.chk_ack(ack)
 
     def write_file_hdr(self):
-        if os.path.exists(LOG_FILE) :
-            created_time = datetime.datetime.fromtimestamp(os.path.getctime(LOG_FILE))
-            backup_file = LOG_FILE + created_time.strftime("_%d%b%Y_%H%M")
+        if os.path.exists(self.stepFile) :
+            created_time = datetime.datetime.fromtimestamp(os.path.getctime(self.stepFile))
+            backup_file = self.stepFile + created_time.strftime("_%d%b%Y_%H%M")
             try:
-                os.rename(LOG_FILE, backup_file)
+                os.rename(self.stepFile, backup_file)
             except WindowsError:
                 os.remove(backup_file)
-                os.rename(LOG_FILE, backup_file)
+                os.rename(self.stepFile, backup_file)
 
-        self.outfile = open(LOG_FILE,"ab+")
+        self.outfile = open(self.stepFile,"ab+")
 
         if self.outfile :
             str = "%10s\t%6s\t%6s\t%6s\t%6s\n" % ("PKT No.", "X", "Y", "Z", "Angle(Degree)")
@@ -289,9 +289,9 @@ class DeviceClient(threading.Thread):
                     dict_log[self.pkt_counter]=[1,2]
                     dict_log[self.pkt_counter][0]=[-x, y, -z, phi]
                 str = "%d, %0.2f, %0.2f, %0.2f, %0.2f\n" % (self.pkt_counter, -x, y, -z, phi)
-                print(dict_log)
-                # with open('dict.txt','w') as f:
-                #     f.write(str(dict_log))
+                #print(dict_log)
+                 with open('dict.txt','w') as f:
+                     f.write(str(dict_log))
                 self.outfile.write(str)
                 self.outfile.flush()
                 queue.put(str)
@@ -423,311 +423,6 @@ class DeviceClient(threading.Thread):
                 self.outfile.close()
         print "STOP"
 
-class DeviceClient2(threading.Thread):
-    client_socket = ''
-    outfile = ''
-    def __init__(self, name, ip, port, queue):
-        threading.Thread.__init__(self)
-        self.host = ip
-        self.port = port
-        self.pkt_counter = 0
-        self.last_ack = ''
-        self.filename = name
-        self.queue = queue
-        self.prev_x = -1
-        self.prev_y = -1
-        self.prev_z = 0
-
-    def open_device(self, host, port):
-        print "Connecting to device", host
-        client_socket = socket.socket()
-        client_socket.connect((host, port))  # connect to the server
-        return client_socket
-
-    def read_device(self, length):
-        device_data = ''
-        # client_socket.flushInput()
-        data_len = 0
-
-        while data_len < length:
-            try:
-                device_data += self.client_socket.recv(length - data_len)
-                data_len += len(device_data)
-                if data_len < length:
-                    time.sleep(0.05)
-            except socket.error:
-                if self.IsDeviceActive() == False:
-                    raise DeviceConnectivityError("Device is not active")
-        return device_data
-
-    def write_device(self, device_data):
-        isSendData = False
-        while isSendData != True:
-            try:
-                self.client_socket.send(device_data)
-                isSendData = True
-            except socket.error:
-                isSendData = False
-                print "write_device: Sending Failure"
-                if self.IsDeviceActive() == False:
-                    raise DeviceConnectivityError("Device is not active")
-
-    def parse_pkt(self, device_data):
-        pkt_info = ()
-        payload = []
-        # print buffer
-        try:
-            (start_code, pkt_num1, pkt_num2, payload_length) = struct.unpack("!BBBB", device_data[0:4])
-
-            if start_code != 0xAA:
-                # print "Error: Failed to detect header at packet start, data=" + device_data.encode("hex")
-                # valid = False
-                raise Exception("Failed to detect header at packet start, data=" + device_data.encode("hex"))
-            else:
-                valid = True
-                (step_count, checksum) = struct.unpack("!HH", device_data[60:64])
-                pkt_info = (pkt_num1, pkt_num2, step_count, checksum)
-                payload = struct.unpack("!ffffffffffffff", device_data[4:60])
-            return valid, pkt_info, payload
-        except Exception as ex:
-            s1 = device_data.encode("hex")
-            print "Exception in parsing: " + ex.message + "\n, data="+s1
-            # elif re.search(b'[\d|\w]+aa.*', s1):  # search and find new packet
-            lst = re.findall(b'[\d|\w]+(aa.*)', s1)
-
-            if lst:
-                strrem = lst[0]
-                lenght = len(strrem) / 2
-                pktrem = device_data[-lenght:]
-                newlen = PACKET_LEN - lenght
-                device_data = self.read_device(newlen)
-                device_data = pktrem + device_data
-            else:
-                device_data = self.read_device(PACKET_LEN)
-            return self.parse_pkt(device_data)
-
-    def create_ack(self, pkt_num1, pkt_num2):
-        ack = []
-        ack.append(1)
-        ack.append(pkt_num1)
-        ack.append(pkt_num2)
-        ack.append((1 + pkt_num1 + pkt_num2) / 256)
-        ack.append((1 + pkt_num1 + pkt_num2) % 256)
-        # print "".join(str('{0:02x}'.format(e)) for e in ack)
-        return "".join(str('{0:02x}'.format(e)) for e in ack).decode("hex")
-
-    def calc_disp(self, sensor_data, theta):
-        d0 = sensor_data[0]
-        d1 = sensor_data[1]
-        d2 = sensor_data[2]
-        d3 = sensor_data[3]
-
-        sin_theta = math.sin(theta)
-        cos_theta = math.cos(theta)
-
-        dx = d0 * cos_theta - d1 * sin_theta
-        dy = d0 * sin_theta + d1 * cos_theta
-        dz = d2
-        dp = d3
-
-        disp = math.sqrt(dx * dx + dy * dy + dz * dz)
-        return dx, dy, dz, dp, disp
-
-    def calc_angle(self, x, y):
-        if (y != 0) and (x != 0):
-            angle = math.atan(x / y)
-            angle = math.degrees(angle)
-        else:
-            if (x == 0):
-                angle = 0
-            else:
-                angle = 90
-        return angle
-
-    def calc_dist(self, x, y, z):
-        r = x * x + y * y + z * z
-        return math.sqrt(r)
-
-    def chk_ack(self, ack):
-        # ack = read_device(4).encode("hex")
-        # print "Received First acknowledgement ",ack.encode("hex")
-        (start_code, state, chksum1, chksum2) = struct.unpack("!BBBB", ack[0:4])
-        if start_code == 0xA0:
-                return True
-        else:
-            s1 = ack.encode("hex")
-            lst = re.findall(b'[\d|\w]+(a0.*)', s1)
-            if lst:
-                strrem = lst[0]
-                lenght = len(strrem) / 2
-                pktrem = ack[-lenght:]
-                newlen = ACK_LEN - lenght
-                ack = self.read_device(newlen)
-                ack = pktrem + ack
-            else:
-                ack = self.read_device(ACK_LEN)
-        return self.chk_ack(ack)
-
-    def write_file_hdr(self):
-        if os.path.exists(LOG_FILE2) :
-            created_time = datetime.datetime.fromtimestamp(os.path.getctime(LOG_FILE2))
-            backup_file = LOG_FILE2 + created_time.strftime("_%d%b%Y_%H%M")
-            try:
-                os.rename(LOG_FILE2, backup_file)
-            except WindowsError:
-                os.remove(backup_file)
-                os.rename(LOG_FILE2, backup_file)
-
-        self.outfile = open(LOG_FILE2,"ab+")
-
-        if self.outfile :
-            str = "%10s\t%6s\t%6s\t%6s\t%6s\n" % ("PKT No.", "X", "Y", "Z", "Angle(Degree)")
-            self.outfile.write(str)
-            self.outfile.flush()
-
-    def write_file(self, x, y, z,phi):
-        dx = self.prev_x - x
-        dy = self.prev_y - y
-        distance = math.sqrt(dx * dx + dy * dy)
-        if (distance > 0.05):
-            if self.outfile :
-                try:
-                    if dict_log[self.pkt_counter][1]==2:
-                        dict_log[self.pkt_counter][1]=[-x, y, -z, phi]
-                except:
-                    dict_log[self.pkt_counter]=[1,2]
-                    dict_log[self.pkt_counter][1]=[-x, y, -z, phi]
-                str = "%d, %0.2f, %0.2f, %0.2f, %0.2f\n" % (self.pkt_counter, -x, y, -z, phi)
-                self.outfile.write(str)
-                self.outfile.flush()
-                queue.put(str)
-                self.prev_x = x
-                self.prev_y = y
-                self.pkt_counter += 1
-
-    def IsDeviceActive(self):
-        status = False
-        try:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(2)
-            result = sock.connect_ex((self.host, self.port))
-            if result == 0:
-                status = True
-                # print "%s" % (self.host)
-                # print sock.getsockname()
-            else:
-                status = False
-            sock.close()
-            # print remoteServerIP + " exit\n"
-        except Exception as ex:
-            status = False
-        return status
-
-    def reconnection(self):
-        try:
-            if self.client_socket :
-                self.client_socket.close()
-        except Exception as ex:
-            print "Socket Closing Error ",str(ex)
-
-        sys.stdout.write("Waiting for reconnection")
-        while self.IsDeviceActive() == False:
-            sys.stdout.write('.')
-        sys.stdout.flush()
-        print "."
-
-        try:
-            self.client_socket = self.open_device(self.host, self.port)
-            self.client_socket.settimeout(TIME_OUT)
-            print "Send last ack=", self.last_ack.encode("hex")
-
-            self.write_device(self.last_ack)
-            # self.sendStartCmd()
-        except socket.error as ex:
-            print "Reconnection: Exception on device connectivity " , str(ex.message)
-            self.reconnection()
-
-    def sendStartCmd(self):
-        self.write_device(START_CMD.decode("hex"))
-        try:
-            ack = self.read_device(ACK_LEN)
-        except DeviceConnectivityError as e:
-            print "Exception on first ack : " + str(e)
-            return
-        self.chk_ack(ack)
-
-    def run(self):
-        # write command to start dead step reckoning
-        try:
-            self.client_socket = self.open_device(self.host, self.port)
-            if self.client_socket:  # if btserial is not null:
-                # global TIME_OUT
-                print self.host, self.port, self.outfile
-                self.write_file_hdr()
-                cmd = START_CMD.decode("hex")
-                self.write_device(cmd)
-                print "Send Start Command: ", START_CMD
-                self.client_socket.settimeout(TIME_OUT)
-                count = 0
-                num_pkts = 0
-                curr_pkt = 0
-                prev_pkt = -1
-                xpos = 0.0  # x-coord in user's reference frame
-                ypos = 0.0  # y-coord in user's reference frame
-                zpos = 0.0  # z-coord in user's reference frame
-                phi = 0.0  # Angular position around Z-axis in user's reference frame
-                try:
-                    ack = self.read_device(ACK_LEN)
-                except DeviceConnectivityError as e:
-                    print "Exception on first ack : "+str(e)
-                    return
-                self.chk_ack(ack)
-
-                # while count < numbytes :
-                last_ack = ''
-                while g_isRunning:
-                    try :
-                        buffer = self.read_device(PACKET_LEN)
-                        valid, packet_info, payload = self.parse_pkt(buffer)
-                        # if not packet_info: break;
-
-                        if (valid == False): continue
-
-                        curr_pkt = packet_info[0] * 256 + packet_info[1]
-
-                        if curr_pkt != prev_pkt:
-                            # print "Read packet # %d...Sending Ack" % curr_pkt
-                            dx, dy, dz, dp, disp = self.calc_disp(payload, phi)
-                            xpos += dx
-                            ypos += dy
-                            zpos += dz
-                            phi += dp
-                            # radial_dist = self.calc_dist(xpos, ypos, zpos)  # for now do not factor in zpos
-                            num_pkts += 1
-                            count += PACKET_LEN
-                            # print count
-                            prev_pkt = curr_pkt
-                            self.write_file(xpos, ypos, zpos, phi)
-
-                        ack = self.create_ack(packet_info[0], packet_info[1])
-                        self.last_ack = ack
-                        self.write_device(ack)
-                    except DeviceConnectivityError as ex:
-                        print "Exception on device connectivity: "+str(ex.message)
-                        self.reconnection()
-
-            cmd = SYS_OFF_CMD.decode("hex")
-            self.write_device(cmd)
-            cmd = PROC_OFF_CMD.decode("hex")
-            self.write_device(cmd)
-            self.client_socket.close()
-            self.outfile.close()
-        except Exception as ex:
-            # print "Exception: "+ str(ex.message)
-            traceback.print_exc()
-            if self.outfile :
-                self.outfile.close()
-        print "STOP"
 
 
 # Create WriteToFile threads
@@ -740,8 +435,8 @@ except:
 
 
 port = 9876
-device_client = DeviceClient(device_ip, device_ip, port, queue)
-device_client2 = DeviceClient2(device_ip2, device_ip2, port, queue)
+device_client = DeviceClient(device_ip, device_ip, port, queue, 'step.txt')
+device_client2 = DeviceClient(device_ip2, device_ip2, port, queue, 'step1.txt')
 device_client.start()
 device_client2.start()
 device_client.join()
